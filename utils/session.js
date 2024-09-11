b6af841ab338529181f56753cfedd169fac9c4c7
Muhamad Aziz Al-asaad <muhazizal.26@gmail.com>
2024-09-11 16:52:24 +0700

    feat: add session utils

---
+++ utils/session.js
@@ -0,0 +1,59 @@
+const jwt = require('jsonwebtoken')
+const { v4: uuidv4 } = require('uuid')
+
+const { Session: SessionModel } = require('../models/index')
+
+exports.createAccessToken = (userId) => {
+	return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1h' })
+}
+
+exports.createRefreshToken = (userId) => {
+	return jwt.sign({ userId }, process.env.JWT_REFRESH_SECRET, { expiresIn: '7d' })
+}
+
+exports.storeAuthSession = async (accessToken, refreshToken) => {
+	try {
+		await SessionModel.sequelize.transaction(async (t) => {
+			await SessionModel.create(
+				{
+					sid: uuidv4(),
+					accessToken,
+					refreshToken,
+				},
+				{ transaction: t }
+			)
+		})
+	} catch (error) {
+		if (!error.statusCode) {
+			error.statusCode = 500
+		}
+		throw error
+	}
+}
+
+exports.destroyAuthSession = async (accessToken) => {
+	try {
+		await SessionModel.sequelize.transaction(async (t) => {
+			await SessionModel.destroy({
+				where: {
+					accessToken,
+				},
+				transaction: t,
+			})
+		})
+	} catch (error) {
+		if (!error.statusCode) {
+			error.statusCode = 500
+		}
+		throw error
+	}
+}
+
+exports.setAccessTokenCookie = (res, accessToken) => {
+	res.cookie('accessToken', accessToken, {
+		httpOnly: true,
+		secure: process.env.NODE_ENV === 'production',
+		sameSite: 'lax',
+		maxAge: 60 * 60 * 1000,
+	})
+}
