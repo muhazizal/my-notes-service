b973289af3d79bdf0e758d347bf022393b8064e3
Muhamad Aziz Al-asaad <muhazizal.26@gmail.com>
2024-09-20 13:54:25 +0700

    fix: change expired token to 10min

--- utils/token.js
+++ utils/token.js
@@ -2,7 +2,7 @@ const crypto = require('crypto')
 
 exports.generateToken = () => {
 	const token = crypto.randomBytes(32).toString('hex')
-	const tokenExpires = Date.now() + 3600000
+	const tokenExpires = Date.now() + 10 * 60 * 1000 // 10 min
 
 	return { token, tokenExpires }
 }
