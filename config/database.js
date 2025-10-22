7021722f5d60ac0e200e7a2ed7b31dccf181e2b9
Muhamad Aziz Al-asaad <muhazizal.26@gmail.com>
2025-10-22 22:42:47 +0700

    fix: database connection pool

--- config/database.js
+++ config/database.js
@@ -2,19 +2,16 @@ const Sequelize = require('sequelize')
 const config = require('./config')[process.env.NODE_ENV]
 
 const isProd = process.env.NODE_ENV === 'production'
-const isPooler =
-	config.host &&
-	(config.host.includes('pooler.supabase.com') ||
-		(config.host.includes('db.') && config.host.includes('.supabase.co')))
+const isPooler = config.host && (config.host.includes('pooler.supabase.com') || config.port == 6543)
 
 const dialectOptions = isPooler
-	? {
+	? {} // Session pooler uses port 6543, NO SSL required
+	: {
 			ssl: {
 				require: true,
 				rejectUnauthorized: false, // required for Supabase direct connection
 			},
 	  }
-	: {}
 
 const sequelize = isProd
 	? new Sequelize(config.url, {
