f7c910404306422f629b9414a7424be4b705d3ee
Muhamad Aziz Al-asaad <muhazizal.26@gmail.com>
2025-10-23 11:48:55 +0700

    fix: config and database

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 module.exports = {
-	url: process.env.DATABASE_URL,
+	uri: process.env.DB_URI,
 	username: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE,
--- config/database.js
+++ config/database.js
@@ -1,14 +1,33 @@
 const Sequelize = require('sequelize')
+const fs = require('fs')
+const path = require('path')
+
 const config = require('./config')
 
-const sequelize = new Sequelize(config.url, {
-	dialect: 'postgres',
-	dialectOptions: {
-		ssl: {
-			require: true,
-			rejectUnauthorized: false,
-		},
-	},
+const caPath = path.resolve(__dirname, './prod-ca-2021.crt')
+const caCert = fs.readFileSync(caPath).toString()
+
+const dialectOptions =
+	process.env.DB_SSL === 'true'
+		? {
+				ssl: {
+					require: true,
+					rejectUnauthorized: false,
+					ca: caCert,
+				},
+		  }
+		: {}
+
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+	host: config.host,
+	port: config.port,
+	dialect: config.dialect,
+	dialectOptions,
 })
+
+sequelize
+	.authenticate()
+	.then(() => console.log('✅ Connection successful'))
+	.catch((err) => console.error('❌ Connection failed:', err))
 
 module.exports = sequelize
